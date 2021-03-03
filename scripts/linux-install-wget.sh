6a6319799921ad4ea432fa5ad0ff3773e5514e48
Kainoa Kanter <44733677+ThatOneCalculator@users.noreply.github.com>
2021-03-02 16:00:45 -0800

    Create linux-install-wget.sh

---
+++ scripts/linux-install-wget.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/ThatOneCalculator/DiscordRPCMaker/releases/download/v1.7.4/drpcm-linux.zip
+unzip ./drpcm-linux.zip
+cd ./linux
+sudo ./install.sh
+cd ..
+rm -rf ./drpcm-linux.zip
+rm -rf ./linux/
