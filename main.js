6a19e2e798fa8c7104fad8d3f312d0cb67a4a4e1
ThatOneCalculator <kainoakanter@gmail.com>
2021-03-06 15:10:59 -0800

    Fix contextIsolation

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
+      contextIsolation: true,
       nodeIntegration: true,
       enableRemoteModule: true,
       preload: path.join(__dirname, "preload.js"),
