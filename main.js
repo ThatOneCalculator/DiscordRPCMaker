3831489cf5b868bb72dab8c9fe83aea4db635eaa
ThatOneCalculator <kainoa@t1c.dev>
2021-03-06 14:34:14 -0800

    Linux desktop

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow, globalShortcut, Notification } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, globalShortcut, Notification } = require("electron")
+const path = require("path")
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -8,33 +8,34 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      preload: path.join(__dirname, "preload.js")
+      preload: path.join(__dirname, "preload.js"),
+      icon: path.join(__dirname, "/assets/icon.png")
     }
   })
   
-  globalShortcut.register('f5', function() {
-      console.log('f5 is pressed')
+  globalShortcut.register("f5", function() {
+      console.log("f5 is pressed")
       win.reload()
   })
-  globalShortcut.register('CommandOrControl+R', function() {
-      console.log('CommandOrControl+R is pressed')
+  globalShortcut.register("CommandOrControl+R", function() {
+      console.log("CommandOrControl+R is pressed")
       win.reload()
   })
 
-  win.loadFile('index.html')
+  win.loadFile("index.html")
   win.setMenu(null)
   
 }
 
 app.whenReady().then(createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit()
   }
 })
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
