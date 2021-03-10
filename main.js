9987c95a1427a97b7fc9e0a36f75c21a4b9ee2c9
KraXen72 <dpelech1@gmail.com>
2021-03-10 21:36:57 +0100

    fix: window height to accomodate logo zoom

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require("path")
 function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
-    height: 600,
+    height: 700,
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: true,
