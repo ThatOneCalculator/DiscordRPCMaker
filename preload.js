3dfd4ad521a7d0cd28fd48d08144b9860bdc1bde
KraXen72 <dpelech1@gmail.com>
2021-03-12 22:44:04 +0100

    feat: most of the preview is done and hooked

--- preload.js
+++ preload.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const os = require('os');
 const dir = `${os.userInfo().homedir}/${process.platform === 'win32' ? '/AppData/Roaming/drpcm/' : '/.config/drpcm/'}`
 
+var clientID = 0
+
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
 }
@@ -17,53 +19,83 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   //clientid enabling
-  document.querySelector(".client-id-enabler").addEventListener("keyup", (e) => {
+  document.querySelector(".client-id-enabler").addEventListener("keyup", async (e) => {
     let inp = document.querySelector(".client-id-enabler")
     let braincell = document.querySelectorAll(".enable-on-clientid")
     if (inp.value !== "" && inp.value.toString().length > 17 && isNaN(parseInt(inp.value)) == false) {
-      let options = fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`).then(options => options.json())
-      console.log(options)
-      braincell.forEach((item, i, arr) => {
-        item.removeAttribute("disabled")
-      })
-    } else {
-      braincell.forEach((item, i, arr) => {
-        item.disabled = true;
-      })
-    }
-    
-  })
+      let response = await fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`)
+      if (response.ok) {
+        let options = await fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`).then(options => options.json())
+        clientid = parseInt(inp.value)//get clientid and store it
+        console.log(options)
+        braincell.forEach((item, i, arr) => {
+          item.removeAttribute("disabled")
+        })
+
+        //galaxybrane datalist thing
+        imageinputs = document.querySelectorAll("#small-image-list, #large-image-list")
+        imageinputs.forEach((item, i, arr) => {
+          htmll = "" //i'll feed this to the innerHTML of the datalist
+          options.forEach((opt, index, array) => {
+            htmll += `<option image-id="${opt.id}">${opt.name}</option>` //i'll add a dummy attribute
+          })
+          item.innerHTML = htmll
+        })
+        //add event listeners to inputs
+        } else {
+          braincell.forEach((item, i, arr) => {
+            item.disabled = true;
+          })
+        }
+      }
+    })
 
   //button enabling
   document.getElementById("button1-enable").addEventListener("change", () => {
     if (document.getElementById("button1-enable").checked) {
+      //enable all buttons
       inps = document.querySelector(".button1")
-      //inps.classList.add("disabled")
       inps.querySelectorAll('input[disabled]').forEach((item, i, arr) => {
         item.removeAttribute("disabled");
       });
+      document.getElementById('button2-enable').removeAttribute('disabled')
+      document.getElementById("preview-button-1").classList.remove("initially-hidden")
+      document.getElementById("button1-input-name").addEventListener("keyup", (event) => {document.getElementById("preview-button-1").innerHTML = event.target.value})
     } else {
+      //disable buttons
       inps = document.querySelector(".button1")
-      //inps.classList.remove("disabled")
       inps.querySelectorAll('input[type="text"]').forEach((item, i, arr) => {
         item.setAttribute("disabled", "");
       });
+
+      button2enable = document.getElementById('button2-enable')
+      document.getElementById("preview-button-1").classList.add("initially-hidden")
+      if (button2enable.checked == true){button2enable.click()}
+      button2enable.disabled = true;
+
+      try {
+        document.getElementById("button1-input-name").removeEventListener("keyup", (event) => {document.getElementById("preview-button-1").innerHTML = event.target.value})
+        document.getElementById("button2-input-name").removeEventListener("keyup", (event) => {document.getElementById("preview-button-2").innerHTML = event.target.value})
+      }catch(e){}
     }
   });
-
   document.getElementById("button2-enable").addEventListener("change", () => {
     if (document.getElementById("button2-enable").checked) {
+      //enable second button
       inps = document.querySelector(".button2")
-      //inps.classList.add("disabled")
       inps.querySelectorAll('input[disabled]').forEach((item, i, arr) => {
         item.removeAttribute("disabled");
       });
+      document.getElementById("preview-button-2").classList.remove("initially-hidden")
+      document.getElementById("button2-input-name").addEventListener("keyup", (event) => {document.getElementById("preview-button-2").innerHTML = event.target.value})
     } else {
       inps = document.querySelector(".button2")
-      //inps.classList.remove("disabled")
+      //disable second button
       inps.querySelectorAll('input[type="text"]').forEach((item, i, arr) => {
         item.setAttribute("disabled", "");
       });
+      document.getElementById("preview-button-2").classList.add("initially-hidden")
+      document.getElementById("button2-input-name").removeEventListener("keyup", (event) => {document.getElementById("preview-button-2").innerHTML = event.target.value})
     }
   });
 
