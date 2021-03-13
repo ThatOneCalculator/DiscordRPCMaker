5eb8e4e3ef585b487caed46e1bbd31d30649ad45
ThatOneCalculator <kainoakanter@gmail.com>
2021-03-12 18:48:22 -0800

    Stuff

--- preload.js
+++ preload.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 const os = require('os');
+const RPC = require('discord-rpc')
+
 const dir = `${os.userInfo().homedir}/${process.platform === 'win32' ? '/AppData/Roaming/drpcm/' : '/.config/drpcm/'}`
 
-var clientID = 0
+let clientID = 0
+
+function validateurl (str) {
+	const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+	return regexp.test(str)
+}
 
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
