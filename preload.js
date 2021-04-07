3fbbb264473abb870282ba8f319c9f1b3a1006ba
KraXen72 <dpelech1@gmail.com>
2021-04-07 20:09:56 +0200

    add: loading presences

--- preload.js
+++ preload.js
@@ -6,7 +6,7 @@ const { dialog, shell, BrowserWindow } = require('@electron/remote')
 const dir = `${os.userInfo().homedir}/${process.platform === 'win32' ? '/AppData/Roaming/drpcm/' : '/.config/drpcm/'}`
 
 let clientID = 0
-let options = {}
+var options = {}
 let client = new RPC.Client({ transport: 'ipc' })
 
 //url validation regex
@@ -95,6 +95,111 @@ function saveAsJson() {
   })
 }
 
+async function bootClientId(presence) {
+  //checks what is in clientid input
+  let inp = document.querySelector(".client-id-enabler")
+  let enableOnClientid = document.querySelectorAll(".enable-on-clientid")
+  enableOnClientid.forEach((item, i, arr) => {
+    item.disabled = true;
+  })
+
+  let button1enable = document.getElementById("button1-enable")
+  let button2enable = document.getElementById("button2-enable")
+  let btn1label = document.getElementById("button1-input-name")
+  let btn1url = document.getElementById("button1-input-url")
+  let btn2label = document.getElementById("button2-input-name")
+  let btn2url = document.getElementById("button2-input-url")
+
+  btn1label.value = ""
+  btn2label.value = ""
+  btn1url.value = ""
+  btn2url.value = ""
+  if (button1enable.checked) {button1enable.click()}
+  document.getElementById("small-image").src = "assets/blank.png"
+
+  //checks if clientid input has a valid clientid (long enough, only numbers)
+  if (inp.value !== "" && inp.value.toString().length > 17 && isNaN(parseInt(inp.value)) == false) {
+    //call discord api to make sure clientid is valid
+    let response = await fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`)
+    if (response.ok) {
+      //client id is valid, enable inputs
+      options = await fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`).then(options => options.json())
+      clientID = inp.value//get clientid and store it
+
+      console.log(options)
+
+      //enable everything on valid clientid
+      enableOnClientid.forEach((item, i, arr) => {
+        item.removeAttribute("disabled")
+      })
+
+      //populate the image input datalists with fetched names of images from the discord api
+      imageinputs = document.querySelectorAll("#small-image-list, #large-image-list")
+      imageinputs.forEach((item, i, arr) => {
+        htmll = ``
+        options.forEach((opt, index, array) => {
+          htmll += `<option image-id="${opt.id}">${opt.name}</option>`
+        })
+        item.innerHTML = htmll
+      })
+
+      //if we are loading a presence
+      console.log(presence)
+      if (Object.keys(presence).length > 0) {
+        //console.log("loading: ", presence)
+
+        let largeimg = document.getElementById("large-image-input")
+        let smallimg = document.getElementById("small-image-input")
+        let desc1 = document.getElementById("description-input-1")
+        let desc2 = document.getElementById("description-input-2")
+      
+
+        //fill in the values from provided presence, and simulate user clicking / typing in the inputs, so the preview updates.
+        largeimg.value = presence.largeimage
+        largeimg.dispatchEvent(new KeyboardEvent("keyup"))
+
+        smallimg.value = presence.smallimage
+        smallimg.dispatchEvent(new KeyboardEvent("keyup"))
+
+        desc1.value = presence.description
+        desc2.value = presence.state
+
+        desc1.dispatchEvent(new KeyboardEvent("keyup"))
+        desc2.dispatchEvent(new KeyboardEvent("keyup"))
+        
+        if (presence.buttons.length > 0) {
+          if (presence.buttons.length == 2) {
+            console.log(" 2 buttons")
+            button1enable.checked = false;
+            button2enable.checked = false;
+            button1enable.click()
+            button2enable.click()
+
+            btn1label.value = presence.buttons[0].label
+            btn1url.value = presence.buttons[0].url
+
+            btn2label.value = presence.buttons[1].label
+            btn2url.value = presence.buttons[1].url
+            
+          } else if (presence.buttons.length == 1) {
+            console.log(" 1 button")
+            button1enable.checked = false;
+            button1enable.click()
+
+            btn1label.value = presence.buttons[0].label
+            btn1url.value = presence.buttons[0].url
+          }
+          btn1label.dispatchEvent(new KeyboardEvent("keyup"))
+          btn2label.dispatchEvent(new KeyboardEvent("keyup"))
+          btn1url.dispatchEvent(new KeyboardEvent("keyup"))
+          btn2url.dispatchEvent(new KeyboardEvent("keyup"))
+        }
+         
+      }
+    }
+  }
+}
+
 //what to do when dom loads
 document.addEventListener("DOMContentLoaded", () => {
   //load presences
@@ -117,44 +222,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   //enable inputs 
-  document.querySelector(".client-id-enabler").addEventListener("keyup", () => { bootClientId() })
-
-  async function bootClientId() {
-    //checks what is in clientid input
-    let inp = document.querySelector(".client-id-enabler")
-    let enableOnClientid = document.querySelectorAll(".enable-on-clientid")
-    enableOnClientid.forEach((item, i, arr) => {
-      item.disabled = true;
-    })
-
-    //checks if clientid input has a valid clientid (long enough, only numbers)
-    if (inp.value !== "" && inp.value.toString().length > 17 && isNaN(parseInt(inp.value)) == false) {
-      //call discord api to make sure clientid is valid
-      let response = await fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`)
-      if (response.ok) {
-        //client id is valid, enable inputs
-        options = await fetch(`https://discord.com/api/oauth2/applications/${inp.value.toString()}/assets`).then(options => options.json())
-        clientID = inp.value//get clientid and store it
-
-        console.log(options)
-
-        //enable everything on valid clientid
-        enableOnClientid.forEach((item, i, arr) => {
-          item.removeAttribute("disabled")
-        })
-
-        //populate the image input datalists with fetched names of images from the discord api
-        imageinputs = document.querySelectorAll("#small-image-list, #large-image-list")
-        imageinputs.forEach((item, i, arr) => {
-          htmll = ``
-          options.forEach((opt, index, array) => {
-            htmll += `<option image-id="${opt.id}">${opt.name}</option>`
-          })
-          item.innerHTML = htmll
-        })
-      }
-    }
-  }
+  document.querySelector(".client-id-enabler").addEventListener("keyup", () => { bootClientId({}) })
 
   function loadSavedPresences() {
     let files = fs.readdir(dir, (directory, files) => {
@@ -177,16 +245,7 @@ document.addEventListener("DOMContentLoaded", () => {
           wrapper.appendChild(elem)
 
           elem.querySelector('.presence-edit').addEventListener("click", () => {
-            document.getElementById("presence-id").value = file.replaceAll(".json", "")
-            document.getElementById("presence-name-input").value = presence.name
-            document.getElementById("clientid-input").value = presence.clientid
-            try { document.getElementById("large-image-input").value = presence.largeimage } catch (e) { }
-            try { document.getElementById("small-image-input").value = presence.smallimage } catch (e) { }
-            try { document.getElementById("description-input-1").value = presence.description } catch (e) { }
-            try { document.getElementById("description-input-2").value = presence.state } catch (e) { }
-            bootClientId()
-            //TODO: buttons
-
+            loadPresence(presence, file)
           })
         }
       })
@@ -251,13 +310,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   //description line 1 updating
-  document.getElementById("description-input-1").addEventListener("keyup", () => {
-    document.getElementById("description-input-1").addEventListener("keyup", (event) => { document.getElementById("preview-description-1").innerHTML = event.target.value })
-  });
+  document.getElementById("description-input-1").addEventListener("keyup", (event) => {
+    document.getElementById("preview-description-1").innerHTML = event.target.value
+ });
 
   //description line 2 updating
-  document.getElementById("description-input-2").addEventListener("keyup", () => {
-    document.getElementById("description-input-2").addEventListener("keyup", (event) => { document.getElementById("preview-description-2").innerHTML = event.target.value })
+  document.getElementById("description-input-2").addEventListener("keyup", (event) => {
+     document.getElementById("preview-description-2").innerHTML = event.target.value
   });
 
   //updating of the large image
@@ -265,7 +324,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let input = document.getElementById("large-image-input")
     let imgname = input.value
     let largeimage = document.getElementById("large-image")
-
     let imgid = getImageIdFromName(imgname)
 
     //if said image doesen't exist, show placeholder
@@ -330,4 +388,12 @@ function registerLinkToOpenInBrowser(elemid, link) {
   elem.addEventListener("click", () => {
     shell.openExternal(link)
   })
+}
+
+function loadPresence(presence, file) {
+  document.getElementById("presence-id").value = file.replaceAll(".json", "")
+  document.getElementById("presence-name-input").value = presence.name
+  document.getElementById("clientid-input").value = presence.clientid
+
+  bootClientId(presence)
 }
