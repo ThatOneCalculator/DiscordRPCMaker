08a46fc239637812161409b600de032d78a19823
KraXen72 <dpelech1@gmail.com>
2021-04-09 20:00:43 +0200

    work: injecting in developer portal

--- clientidDetect.js
+++ clientidDetect.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
 document.addEventListener("DOMContentLoaded", () => {
-    if (window.location.href == "https://discord.com/developers/applications") {
+    if (window.location.href =="https://discord.com/developers/applications") {
         makeflyout("Go to your desired application, and the Client ID will be detected automatically! ", "drpcm-hint-flyout")
     }
+    setTimeout(() => {hideUnwanted()}, 1000)
 })
 document.addEventListener("mousemove", () => {
     let prevflyout = document.querySelector("#drpcm-hint-flyout")
@@ -22,7 +23,6 @@ document.addEventListener("mousemove", () => {
 function makeflyout(html, id) {
     let flyout = document.createElement("div")
     flyout.id = id
-
     flyout.innerHTML = html + `<button style = "color: white; cursor: pointer; font-weight: bold;" onclick="this.parentElement.style.display = 'none'">&times;</button>`
     flyout.style.position = "absolute"
     flyout.style.zIndex = "999"
@@ -34,4 +34,19 @@ function makeflyout(html, id) {
     flyout.style.padding = "1rem"
 
     document.body.appendChild(flyout)
+}
+
+function hideUnwanted() {
+    let elems = [
+        document.querySelector(".wrapper-3aJbIC")
+    ]
+    elems.forEach(elem => elem.remove())
+
+    document.querySelector(".flush-zNaLgf").innerText = "Click 'New Application' to create a new presence, or select an already created one below"
+    document.querySelector(".wordmark-1G98vs").innerText = "Discord RPC Developer Portal"
+
+    document.querySelector(".button-38aScr").addEventListener("click", () => {
+        let aaa = document.getElementsByClassName('medium-zmzTW- weightNormal-3CX1dN')
+        aaa[0].innerText = "This is what your presence will be called."
+    })
 }
--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ const path = require('path');
 const RPC = require('discord-rpc')
 const openExplorer = require('open-file-explorer');
 const { dialog, shell, BrowserWindow } = require('@electron/remote')
+const slash = os.platform() == 'win32' ? "\\" : "/"
 
 const dir = `${os.userInfo().homedir}/${process.platform === 'win32' ? '/AppData/Roaming/drpcm/' : '/.config/drpcm/'}`
 
@@ -16,9 +17,9 @@ let inputs = []
 //url validation regex
 function validateurl(str) {
   const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+  
   return regexp.test(str)
 }
-
 function dec2hex(dec) {
   return dec.toString(16).padStart(2, "0")
 }
@@ -478,9 +479,9 @@ document.addEventListener("DOMContentLoaded", () => {
       height: 700,
       webPreferences: {
         nodeIntegration: true,
-        preload: `${__dirname}/clientidDetect.js`
+        preload: `${__dirname}${slash}clientidDetect.js`
       }
-    });F
+    });
     //load html into window
     develWindow.loadURL('https://discord.com/developers');
     develWindow.webContents.openDevTools();
