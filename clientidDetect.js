2caf2dcc628cef57fc0473ddcae8d53dd2aa16d7
ThatOneCalculator <kainoa@t1c.dev>
2021-04-09 09:04:34 -0700

    Grammar

--- clientidDetect.js
+++ clientidDetect.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 document.addEventListener("DOMContentLoaded", () => {
     if (window.location.href == "https://discord.com/developers/applications") {
-        makeflyout("Go to your desired application, and the ClientID will be detected automatically! ", "drpcm-hint-flyout")
+        makeflyout("Go to your desired application, and the Client ID will be detected automatically! ", "drpcm-hint-flyout")
     }
 })
 document.addEventListener("mousemove", () => {
@@ -11,9 +11,9 @@ document.addEventListener("mousemove", () => {
 
     if (clientid !== null && fly == null) {
         let id = clientid.innerText
-        if (prevflyout !==  null){prevflyout.remove()}
+        if (prevflyout !== null) { prevflyout.remove() }
         makeflyout(`
-        detected clientID!
+        Detected Client ID!
         <code style = "font-family: monospace; background: #313131; padding: 5px; border-radius: 5px;">${id}</code>
     `, "drpcm-success-flyout")
     }
@@ -21,17 +21,17 @@ document.addEventListener("mousemove", () => {
 
 function makeflyout(html, id) {
     let flyout = document.createElement("div")
-        flyout.id = id
+    flyout.id = id
 
-        flyout.innerHTML = html + `<button style = "color: white; cursor: pointer; font-weight: bold;" onclick="this.parentElement.style.display = 'none'">&times;</button>`
-        flyout.style.position = "absolute"
-        flyout.style.zIndex = "999"
-        flyout.style.bottom = "0"
-        flyout.style.right = "0"
-        flyout.style.margin = "1rem"
-        flyout.style.background = "#202225"
-        flyout.style.borderRadius = "5px"
-        flyout.style.padding = "1rem"
+    flyout.innerHTML = html + `<button style = "color: white; cursor: pointer; font-weight: bold;" onclick="this.parentElement.style.display = 'none'">&times;</button>`
+    flyout.style.position = "absolute"
+    flyout.style.zIndex = "999"
+    flyout.style.bottom = "0"
+    flyout.style.right = "0"
+    flyout.style.margin = "1rem"
+    flyout.style.background = "#202225"
+    flyout.style.borderRadius = "5px"
+    flyout.style.padding = "1rem"
 
-        document.body.appendChild(flyout)
+    document.body.appendChild(flyout)
 }
