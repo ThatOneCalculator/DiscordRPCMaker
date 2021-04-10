070588c65345527d186645c48a790b9f8ec0724f
ThatOneCalculator <kainoa@t1c.dev>
2021-04-09 18:59:56 -0700

    Done: Developer Portal Editing

--- clientidDetect.js
+++ clientidDetect.js
@@ -3,23 +3,27 @@ const electron = require('electron');
 document.addEventListener("DOMContentLoaded", () => {
     if (window.location.href == "https://discord.com/developers/applications") {
         makeflyout("Go to your desired application, and the Client ID will be detected automatically! ", "drpcm-hint-flyout")
-        // try { setTimeout(() => { document.querySelector(".wrapper-36iaZw").remove() }, 500) } catch (e) { console.log(e) }
     }
-    try { setTimeout(() => { hideUnwanted() }, 500) } catch (e) { }
-    try { setTimeout(() => { document.querySelector(".wrapper-3aJbIC").remove() }, 500) } catch (e) { }
+    try { setTimeout(() => { modifiyText() }, 1000) } catch (e) { }
+    try { setTimeout(() => { hideUnwanted() }, 1000) } catch (e) { }
+    try { setTimeout(() => { document.querySelector(".wrapper-3aJbIC").style.display = "none" }, 2000) } catch (e) { }
+    setTimeout(() => { abortChildren() }, 1000)
 })
+
 document.addEventListener("mousemove", () => {
     let prevflyout = document.querySelector("#drpcm-hint-flyout")
     let clientid = document.querySelector(".code-j7WzRK")
     let fly = document.querySelector("#drpcm-success-flyout")
     if (clientid !== null && fly == null) {
         let id = clientid.innerText
-        if (prevflyout !== null) { prevflyout.remove() }
+        if (prevflyout !== null) { prevflyout.style.display = "none" }
         makeflyout(`
         Detected Client ID!
         <code style = "font-family: monospace; background: #313131; padding: 5px; border-radius: 5px;">${id}</code>
     `, "drpcm-success-flyout")
     }
+    abortChildren()
+    modifiyText()
 })
 
 function makeflyout(html, id) {
@@ -34,23 +38,10 @@ function makeflyout(html, id) {
     flyout.style.background = "#202225"
     flyout.style.borderRadius = "5px"
     flyout.style.padding = "1rem"
-
     document.body.appendChild(flyout)
 }
 
-function hideUnwanted() {
-    let elems = [
-        document.querySelector(".wrapper-3aJbIC"),
-        document.querySelector(".content-32JuGj"),
-        document.querySelector(".wrapperInner-2p1_wN"),
-        // document.querySelector("href[https://discord.com/developers/teams]"),
-        // document.querySelector("href[https://discord.com/developers/servers]"),
-        // document.querySelector("href[https://discord.com/developers/docs]")
-    ]
-    elems.forEach(elem => {
-        try { setTimeout(() => elem.remove(), 500) } catch (e) { }
-    })
-
+function modifiyText() {
     document.querySelector(".flush-zNaLgf").innerText = "Click 'New Application' to create a new presence, or select an already created one below"
     document.querySelector(".wordmark-1G98vs").innerText = "Discord RPC Developer Portal"
     let clientid = document.querySelector(".code-j7WzRK")
@@ -58,5 +49,39 @@ function hideUnwanted() {
         let aaa = document.getElementsByClassName('medium-zmzTW- weightNormal-3CX1dN')
         aaa[0].innerText = "This is what your presence will be called."
     })
-    try { setTimeout(() => { document.querySelector(".wrapper-3aJbIC").remove() }, 2000) } catch (e) { }
+}
+
+function abortChildren() {
+    document.getElementsByClassName("wrapper-3S5Jvh backToLink-UqPo19")[0].style.display = "none"
+    let optionsList = document.querySelector(".mainList-otExiM")
+    optionsList.childNodes[1].style.display = "none"
+    optionsList.childNodes[2].style.display = "none"
+    optionsList.childNodes[4].style.display = "none"
+    let subOptionsList = document.querySelector(".subList-3zlU-5")
+    subOptionsList.childNodes[1].style.display = "none"
+    document.querySelector(".marginBottomMedium-3rCQQt").style.display = "none"
+    otherBlocks = [
+        document.getElementsByClassName("flex-1xMQg5 flexHorizontal-1YWL8b flexJustifyStart-1R2n-N flexAlignStretch-1aueRm flexWrap-1K8nA-")[0],
+        document.getElementsByClassName("headingContent-2bBpIC bottomMarginMedium-2YVTXf")[0],
+        document.getElementsByClassName("heading-3MvkF0 marginBottom20-QW1wVs medium-zmzTW- size16-1__VVI height20-13xN5Z primary300-qtIOwv weightNormal-3CX1dN")[0]
+    ]
+    otherBlocks.forEach(elem => { elem.style.display = "none" })
+    setTimeout(() => { document.querySelector("wrapper-3aJbIC") }, 2000)
+}
+
+function hideUnwanted() {
+    let elems = [
+        // document.querySelector(".marginBottomMedium-3rCQQt"),
+        // document.querySelector(".marginBottomMedium-3rCQQt")
+    ]
+    elems.forEach(elem => {
+        try { setTimeout(() => elem.style.display = "none", 1000) } catch (e) { }
+    })
+    try { setTimeout(() => { document.querySelector(".wrapper-3aJbIC").style.display = "none" }, 2000) } catch (e) { }
+}
+
+function hideFormElements() {
+    let a = document.querySelector(".contentWrapperInner-15LzPz")
+    a.childNodes[0].querySelector(".marginBottomMedium-3rCQQt").remove()
+    a.childNodes[0].querySelector(".marginBottomMedium-3rCQQt").remove()
 }
