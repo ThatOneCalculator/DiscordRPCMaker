7c27381c3e1592fc7ca9939ecbe205ba021fe48e
SenseiTemp <96332610+SenseiTemp@users.noreply.github.com>
2021-12-18 02:43:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -33,12 +33,12 @@ if (id !== undefined) {
 	if (options.largeimage !== '') {
 		activity.largeImageKey = options.largeimage
 		// If you change this and some asks about this, please still give me credit :)
-		activity.largeImageText = "Made with ThatOneCalculator's Discord RPC Maker (v2.1.1 CLI)!"
+		activity.largeImageText = "Rest in peace."
 	}
 	if (options.smallimage !== '') {
 		activity.smallImageKey = options.smallimage
 		// Same applies with assets.large_text
-		activity.smallImageText = 'https://drpcm.t1c.dev/'
+		activity.smallImageText = "Seizure Giver 3000's"
 	}
 	if (assets !== {}) { activity.assets = assets }
 	if (options.description !== '') { activity.details = options.description }
