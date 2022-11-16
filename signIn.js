eda2ff620696bba20be62fabc560fb4e7549f00d
xhayper <hayper1919@gmail.com>
2022-11-16 20:40:58 +0700

    feat: properly update all dependencies

--- signIn.js
+++ signIn.js
@@ -1,27 +1,28 @@
-const electron = require('electron')
-// const keytar = require('keytar')
-
 document.addEventListener("DOMContentLoaded", () => {
-    const targetNode = document.body;
+  const targetNode = document.body;
 
-    // Options for the observer (which mutations to observe)
-    let config = { characterData: true, attributes: true, childList: true, subtree: true };
+  // Options for the observer (which mutations to observe)
+  let config = {
+    characterData: true,
+    attributes: true,
+    childList: true,
+    subtree: true,
+  };
 
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
 
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
-
-})
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+});
 
 document.addEventListener("click", () => {
-    callback()
-})
+  callback();
+});
 
-const callback = function (mutationsList) {
-    console.log("change")
-    if (document.getElementById("display_result").value !== "") {
-        console.log()
-    }
-}
+const callback = () => {
+  console.log("change");
+  if (document.getElementById("display_result").value !== "") {
+    console.log();
+  }
+};
